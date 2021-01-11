33451fb053d2bda93b8b284ea0c11196ba37a0c0
edz <1549885265@qq.com>
2021-01-11 12:00:42 +0800

    ESLint+Prettier知识点总结

---
+++ Frame/ESLint+Prettier/eslint-demo/src/lib/source.js
@@ -0,0 +1,14 @@
+function func() {
+    window.alert('hujnbhj');
+    global.value1 = 'hjuikjnhji';
+
+    var1 = '7890';
+    // var2 = '7890';
+
+    const set1 = new Set();
+    console.log('set', set1);
+}
+
+func();
+
+export default func;
